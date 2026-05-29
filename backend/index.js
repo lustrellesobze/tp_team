2b366f1b17cacee9d8a4a43d493c362177d4e89f
Carlex <carmanda@outlook.fr>
2026-05-29 07:20:14 +0100

    fix(backend): forcer dotenv à chercher le fichier .env dans le dossier courant (pour Phusion Passenger)

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: require('path').join(__dirname, '.env') });
 const { getPool, initDatabase } = require("./db");
 const { createApp } = require("./createApp");
 
